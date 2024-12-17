3513069b981d61e084083be0e934870a61874076
jyothiswarJB <itzeshwarking@gmail.com>
2024-12-17 11:44:33 +0530

    files added

---
+++ oprator.js
@@ -0,0 +1,7 @@
+var a = 46;
+var b =5;
+console.log(a++)
+console.log(a-b)
+console.log(a*b)
+console.log(a%b)
+console.log(a**8)
---
+++ premutivedata.js
@@ -0,0 +1,6 @@
+let employee = "raj";
+console.log(typeof employee)
+let profit;
+console.log (typeof profit)
+let person ={name:"sree",age:23,employed:true,salary:28000};
+console.log(person.salary);
